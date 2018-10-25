ad9c096041e65af04a7d2fd704a0e1f6608b9283
Theresa Eliger <910theresa@gmail.com>
2018-10-24 20:11:31 -0700

    Added functionality for metadata in JSON format.

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ var multer = require('multer');
 var fileUpload = multer({dest: 'uploadedFiles/'});
 var app = module.exports = express();
 app.use(bodyParser.json());
-
 app.use(express.static(__dirname + '/static'));
-
 
 app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.post('/uploadfile', fileUpload.single('file'), () => {
-    console.log('file upload requested');
+app.post('/uploadfile', fileUpload.single('file'), (req, res, next) => {
+    console.log('File has been uploaded!');
+    console.log('request:', req);
+    return res.json({
+        name: req.file.originalname,
+        size: req.file.size,
+        type: req.file.mimetype
+    });
 })
 
+// Set port to 8000
 app.listen(8000, () => {
     console.log('Listening on localhost:8000...')
 })
